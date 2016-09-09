7afbc7042652ab468208156efb2237b2301a88b5
Enrique Munoz de Cote <enrique@Enriques-MacBook.local>
2016-09-09 01:32:52 -0500

    initial commit

---
+++ experiment1.py
@@ -0,0 +1,18 @@
+import environment.gridWorld as gw
+import learner.DQN as q
+
+def main(argv=None):
+    state = gw.initGridRand()
+    print(gw.dispGrid(state))
+    state = gw.makeMove(state, 1)
+    state = gw.makeMove(state, 1)
+    state = gw.makeMove(state, 1)
+    state = gw.makeMove(state, 3)
+    print('Reward: %s' % (gw.getReward(state),))
+    print(gw.dispGrid(state))
+
+    print(q.model.predict(state.reshape(1, 64), batch_size=1))
+    # just to show an example output; read outputs left to right: up/down/left/right
+
+if __name__ == '__main__':
+    main()
